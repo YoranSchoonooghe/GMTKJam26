a4bc0a49e9739a4f9989f0b220cfd6f48598791f
YoranSchoonooghe <yoran.schoonooghe@student.howest.be>
2026-07-22 15:38:29 +0200

    Added GameMode, PlayerController, PlayerCharacter

---
+++ Source/GMTKJam26/GameModeGMTK.cpp
@@ -0,0 +1,2 @@
+#include "GameModeGMTK.h"
+
---
+++ Source/GMTKJam26/GameModeGMTK.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GameModeGMTK.generated.h"
+
+UCLASS()
+class GMTKJAM26_API AGameModeGMTK : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
